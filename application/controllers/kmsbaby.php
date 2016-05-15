90a2cac3e4e847e6dbb69ebc94a936f23359a8ae
JaservTECH <jafarabdurrahmanal-basyir@hotmail.com>
2016-05-15 16:05:09 +0700

    Penambahan KMS
    KMS baby wkwkwk

---
+++ application/controllers/kmsbaby.php
@@ -0,0 +1,20 @@
+<?php 
+if (!defined ("BASEPATH"))
+	exit ('You dont have access to this content');
+
+class Kmsbaby extends CI_Controller {
+	function __CONSTRUCT(){
+		parent::__CONSTRUCT();
+		$this->load->helper('url');
+	}
+	function index() {
+		$temp=array(12,12.4,14,15,16,17,16);
+		$temp2 ="";
+		foreach ($temp as $value){
+			$temp2.=$value.",";
+		}
+		$temp2 = substr($temp2,0,strlen($temp2)-1);
+		$data['dataArray']= $temp2;
+		$this->load->view('kms_tampilanbaby',$data);
+	}
+}
