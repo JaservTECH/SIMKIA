0e598bcbff8b10b1c8b68534a122d8222bfd83ff
Jafar Abdurrahman Albasyir <jafarabdurrahmanal-basyir@hotmail.com>
2016-05-19 21:09:37 +0700

    annisa maafin ya telat
    ini sudah saya coba maaf ndak perfect

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,9 +2,25 @@
 
 	Class Home extends CI_Controller
 	{
+		public function __CONSTRUCT(){
+			parent::__CONSTRUCT();
+			$this->load->library('session');
+			$this->load->helper('url');
+		}
 		public function index(){
-			$this->load->view("header_tampilan");
+			$temp = $this->session->all_userdata();
+			if(!array_key_exists("pasien",$temp))
+				redirect("Login/");
+			if(!array_key_exists("pass",$temp))
+				redirect("Login/");
+			$data['keluar'] = base_url()."index.php/Home/keluarPasien";
+			$this->load->view("header_tampilan",$data);
 			$this->load->view("home_tampilan");
 		}
-		
+		public function keluarPasien(){
+			
+			$this->session->unset_userdata('pasien');
+			$this->session->unset_userdata('pass');
+			redirect("Login/");
+		}
 }
--- application/controllers/catatan_nifas.php
+++ application/controllers/catatan_nifas.php
@@ -14,86 +14,86 @@
 			$this->load->model('data_pasien_model');
 			$temp = $this->kunjungan_model->GetAllContentTable();
 			$content='';
+			$code = '';
+			$cursor = count($temp);
+			$cursor_now = 1;
 			foreach ($temp as $key => $value){
-				$content.="<tr>";
-				$content.="<td>";
-				$content.=$value['tanggal_kunjungan'];
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 22)
-					$content.=$value['hasil_pemeriksaan']." mmHg";
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 36)
-					$content.=$value['hasil_pemeriksaan']."/menit";
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 35)
-					$content.=$value['hasil_pemeriksaan']."/menit";
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 37)
-					$content.=$value['hasil_pemeriksaan']." C";
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 38)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 41)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 42)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 39)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 40)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 43)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 33)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="<td>";
-				if(intval($value['id_jenis_pemeriksaan'])== 34)
-					$content.=$value['hasil_pemeriksaan'];
-				else
-					$content.="-";
-				$content.="</td>";
-				$content.="</tr>";
+				if($code == ""){
+					$code = $value['tanggal_kunjungan'];
+					$codd[22] = "<td>-</td>";	
+					$codd[36] = "<td>-</td>";	
+					$codd[35] = "<td>-</td>";	
+					$codd[37] = "<td>-</td>";	
+					$codd[38] = "<td>-</td>";	
+					$codd[41] = "<td>-</td>";	
+					$codd[42] = "<td>-</td>";	
+					$codd[39] = "<td>-</td>";	
+					$codd[40] = "<td>-</td>";	
+					$codd[43] = "<td>-</td>";	
+					$codd[33] = "<td>-</td>";	
+					$codd[34] = "<td>-</td>";	
+				}else if($code != $value['tanggal_kunjungan']){
+					$content.="<tr>";
+					$content.="<td>".$code."</td>";
+					$content.=$codd[22];
+					$content.=$codd[36];
+					$content.=$codd[35];
+					$content.=$codd[37];
+					$content.=$codd[38];
+					$content.=$codd[41];
+					$content.=$codd[42];
+					$content.=$codd[39];
+					$content.=$codd[40];
+					$content.=$codd[43];
+					$content.=$codd[33];
+					$content.=$codd[34];
+					$content.="</tr>";
+					$code = $value['tanggal_kunjungan'];
+					$codd[22] = "<td>-</td>";	
+					$codd[36] = "<td>-</td>";	
+					$codd[35] = "<td>-</td>";	
+					$codd[37] = "<td>-</td>";	
+					$codd[38] = "<td>-</td>";	
+					$codd[41] = "<td>-</td>";	
+					$codd[42] = "<td>-</td>";	
+					$codd[39] = "<td>-</td>";	
+					$codd[40] = "<td>-</td>";	
+					$codd[43] = "<td>-</td>";	
+					$codd[33] = "<td>-</td>";	
+					$codd[34] = "<td>-</td>";	
+				}
+				if(intval($value['id_jenis_pemeriksaan'])== 22)	$codd[22] = "<td>".$value['hasil_pemeriksaan']." mmHg"."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 36)	$codd[36] = "<td>".$value['hasil_pemeriksaan']." /menit"."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 35)	$codd[35] = "<td>".$value['hasil_pemeriksaan']." /menit"."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 37)	$codd[37] = "<td>".$value['hasil_pemeriksaan']." C"."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 38)	$codd[38] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 41)	$codd[41] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 42)	$codd[42] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 39)	$codd[39] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 40)	$codd[40] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 43)	$codd[43] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 33)	$codd[33] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if(intval($value['id_jenis_pemeriksaan'])== 34)	$codd[34] = "<td>".$value['hasil_pemeriksaan']."</td>";
+				if($cursor_now == $cursor){
+					$content.="<tr>";
+					$content.="<td>".$code."</td>";
+					$content.=$codd[22];
+					$content.=$codd[36];
+					$content.=$codd[35];
+					$content.=$codd[37];
+					$content.=$codd[38];
+					$content.=$codd[41];
+					$content.=$codd[42];
+					$content.=$codd[39];
+					$content.=$codd[40];
+					$content.=$codd[43];
+					$content.=$codd[33];
+					$content.=$codd[34];
+					$content.="</tr>";
+				}else{
+					$cursor_now+=1;
+				}
 			}
-			
 			$data['content']=$content;
 			$this->load->view('catatan_nifas_tampilan',$data);
 		}
